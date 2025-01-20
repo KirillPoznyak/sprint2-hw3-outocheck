476725f65852fc06d31f6bb6bd6aff5ca74085ba
kirill poznyak <k.paznyak@gmail.com>
2025-01-20 16:59:35 +0300

    Create project of hw3 with autocheck and complete 1st task

---
+++ lesson-03/01.js
@@ -0,0 +1,7 @@
+// Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+
+function calculateAverage(number1, number2) {
+    return (number1 + number2) / 2;
+};
+let arithmetic = calculateAverage(5, 10);
+console.log('Среднее арифметическое: ', arithmetic);
